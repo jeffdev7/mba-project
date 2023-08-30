6253a6e13c6eed33edcf1eb4d488e3902099ddc4
Jefferson <73981595+jeffdev7@users.noreply.github.com>
2023-08-30 18:50:06 -0300

    It adds the Inversion of Control layer

---
+++ fast-booze/fast-booze.IoC/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using fast_booze.data.DBConfiguration;
+using fast_booze.data.Repositories;
+using fast_booze.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace fast_booze.IoC
+{
+    public sealed class Bootstrapper
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            service.AddScoped<IBeverageRepository, BeverageRepository>();
+            service.AddScoped<ICustomerRepository, CustomerRepository>();
+            service.AddScoped<IItemOrderRepository, ItemOrderRepository>();
+            service.AddScoped<ILiquorStoreRepository, LiquorStoreRepository>();
+            service.AddScoped<IOrderRepository, OrderRepository>();
+            service.AddScoped<IStockRepository, StockRepository>();
+
+
+
+            service.AddTransient<IAppDbContext, ApplicationContext>();
+        }
+    }
+}
